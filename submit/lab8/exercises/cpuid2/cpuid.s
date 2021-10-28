2c2dc6211dae3e3eb8ae5dd0e81297925b97fad4
Daniel Orbach <dorbach1@cs.binghamton.edu>
2021-10-28 11:11:53 -0400

    finished lab8

--- submit/lab8/exercises/cpuid2/cpuid.s
+++ submit/lab8/exercises/cpuid2/cpuid.s
@@ -6,14 +6,15 @@ get_cpuid:
 	pushq   %rcx
 	pushq   %rdx
 	xorl 	%eax, %eax	#setup cpuid opcode to 0
+	movl    %edi, %eax	#moving opcode into correct place
 	cpuid
 	#largest param in %eax
 	#12-char manufacturer string in ebx, edx, ecx.
-	movl	%eax, (%rdi)	#store eax cpuid result
-	movl	%ebx, (%rsi)	#store ebx cpuid result
+	movl	%eax, (%rsi)	#store eax cpuid result
+	popq    %rax
+	movl	%ebx, (%rax)	#store ebx cpuid result
 	popq	%rax		#pop address for edxP
-	movl    %edx, (%rax)    #store edx cpuid result
-	popq    %rax		#pop address for ecxP
-	movl	%ecx, (%rax)	#store ecx cpuid result
+	movl    %ecx, (%rax)    #store ecx cpuid result previous pointer ecx
+	movl    %edx, (%r8)     #Store edx result in r8
 	ret
 	
--- submit/lab8/exercises/parity/parity.s
+++ submit/lab8/exercises/parity/parity.s
@@ -2,7 +2,13 @@
 	.globl get_parity
 #edi contains n	
 get_parity:
+	xorl %eax, %eax 	#setting eax to 0 
+	testl %edi, %edi	#setting parity flag
+	jpe .C2			#IF parity is odd jump to c2 and add 1 to eax		
 
-	#@TODO: add code here to set eax to 1 iff edi has even parity
+.C1:
 	ret
-	
+
+.C2:
+	add $1, %eax
+	jmp .C1	
